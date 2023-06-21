03404e4e84b1964b9d995b65839a7e3a28ab71d0
Lucas Capart <lucas.capart80@gmail.com>
2023-06-21 18:07:34 +0200

    Update app.js
    suppression des anciens Middleware

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { User, Book } = require('./models/thing')
+const cors = require('cors');
 
-const app = express();
+const authRoutes = require('./routes/auth')
+const bookRoutes = require('./routes/books')
 
 mongoose.connect('mongodb+srv://lucascapart80:KWwuTzdgRbmIVQnP@cluster0.cuuwr1l.mongodb.net/?retryWrites=true&w=majority',
     {
@@ -12,7 +14,12 @@ mongoose.connect('mongodb+srv://lucascapart80:KWwuTzdgRbmIVQnP@cluster0.cuuwr1l.
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+const app = express();
+
 app.use(express.json());
+app.use(cors());
+
+console.log('test')
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,39 +28,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-});
 
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.use('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/books', bookRoutes);
 
 module.exports = app;
