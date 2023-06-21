9934fec4c2ccb2bc557fc6554b3123678b8892aa
Lucas Capart <lucas.capart80@gmail.com>
2023-06-21 18:06:59 +0200

    Update Book.js
    Ajout du 'genre' et 'ratings'

--- Backend/models/Book.js
+++ Backend/models/Book.js
@@ -6,6 +6,8 @@ const bookSchema = mongoose.Schema({
     author: { type: String, required: true },
     imageUrl: { type: String, required: true },
     year: { type: Number, required: true },
+    genre: { type: String, required: true },
+    ratings: [{ userId: String, grade: Number, averageRating: Number }],
 });
 
 module.exports = mongoose.model('Book', bookSchema);
