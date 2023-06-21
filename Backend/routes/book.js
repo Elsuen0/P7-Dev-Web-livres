bca40a1d710b9cfb7d06692e0afee8f0f7fc3355
Lucas Capart <lucas.capart80@gmail.com>
2023-06-21 18:06:12 +0200

    Delete book.js
    Suppression de l'ancienne route 'book'

--- Backend/routes/book.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bookCtrl = require('../controllers/book')
-
-router.post('/books', bookCtrl.addBook);
-
-module.exports = router;
