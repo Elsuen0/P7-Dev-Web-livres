ab79e869388eaa15f8d522b1215665fc7136c466
Lucas Capart <lucas.capart80@gmail.com>
2023-06-21 18:07:16 +0200

    Update book.js
    Modification du controller book

--- Backend/controllers/book.js
+++ Backend/controllers/book.js
@@ -2,10 +2,25 @@ const Book = require("../models/Book");
 
 exports.addBook = (req, res, next) => {
     delete req.body._id;
+    console.log('ceci est un test')
+
+    console.log(res)
+
     const book = new Book({
         ...req.body
     });
     book.save()
         .then(() => res.status(201).json({ message: 'Livre ajouté !' }))
         .catch(error => res.status(400).json({ error }));
+};
+
+exports.displayBooks = (req, res, next) => {
+    Book.find()
+        .then((books) => {
+            res.json(books);
+        })
+        .catch((error) => {
+            next(error);
+        });
+
 };
