b7e3ba2ce40a84bbea0d0ee242b7091d7da88268
Lucas Capart <lucas.capart80@gmail.com>
2023-06-28 15:03:49 +0200

    Update books.js
    Ajout des routes 'Post + get + delete' & const auth + const multer + const book

--- Backend/routes/books.js
+++ Backend/routes/books.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const bookCtrl = require('../controllers/book');
 
-router.post('/', bookCtrl.addBook);
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const book = require('../models/book');
+
+router.post('/', auth, multer, bookCtrl.addBook);
 router.get('/', bookCtrl.displayBooks);
+router.get('/bestrating', bookCtrl.getBooksByBestRating);
+router.get('/:id', bookCtrl.getBookById);
+router.delete('/:id', auth, bookCtrl.deleteBooks);
+
 
 module.exports = router;
