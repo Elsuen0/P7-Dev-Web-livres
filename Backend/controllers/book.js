991ca49935e094c36a904c0d4491972f253652b0
Lucas Capart <lucas.capart80@gmail.com>
2023-06-29 17:45:29 +0200

    Update book.js
    Mise à jour du controller book.js

--- Backend/controllers/book.js
+++ Backend/controllers/book.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 const Book = require('../models/book');
-const book = require('../models/book');
+const fs = require('fs');
 
 exports.addBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
     delete bookObject._userId;
-    const thing = new Book({
+    const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
 
-    thing.save()
+    book.save()
         .then(() => { res.status(201).json({ message: 'Objet enregistré !' }) })
         .catch(error => { res.status(400).json({ error }) })
 };
@@ -32,7 +32,7 @@ exports.displayBooks = (req, res, next) => {
 
 exports.getBookById = (req, res, next) => {
     const bookId = req.params.id;
-
+    console.log(bookId, 'test')
     Book.findById(bookId)
         .then((book) => {
             if (!book) {
@@ -45,40 +45,44 @@ exports.getBookById = (req, res, next) => {
         });
 };
 
-exports.getBooksByBestRating = (req, res, next) => {
-    Book.find()
-        .sort({ 'ratings.averageRating': -1 })
-        .limit(3)
-        .then((books) => {
-            res.json(books);
-        })
-        .catch((error) => {
-            next(error);
-        });
+
+exports.updateOne = (req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+        .catch(error => res.status(400).json({ error }));
 };
 
 
 exports.deleteBooks = (req, res, next) => {
-    const bookId = req.params.id;
-
-    Book.findByIdAndRemove(bookId)
-        .then((book) => {
-            if (!book) {
-                return res.status(404).json({ message: 'Livre non trouvé.' });
-            }
-
-            // Supprimer l'image associée
-            if (book.imageUrl) {
-                const imagePath = path.join(__dirname, '..', 'images', book.imagesUrl);
-                fs.unlink(imagePath, (err) => {
-                    if (err) {
-                        console.error('Erreur lors de la suppression de l\'image :', err);
-                    }
+    Book.findOne({ _id: req.params.id })
+        .then(Book => {
+            if (Book.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Not authorized' });
+            } else {
+                const filename = Book.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Book.deleteOne({ _id: req.params.id })
+                        .then(() => { res.status(200).json({ message: 'Objet supprimé !' }) })
+                        .catch(error => res.status(401).json({ error }));
                 });
             }
-            res.json({ message: 'Livre supprimé.' });
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+};
+
+exports.getBestRatedBooks = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .sort({ averageRating: -1 })
+        .limit(3)
+        .exec()
+        .then((books) => {
+            res.status(200).json({ books });
         })
         .catch((error) => {
-            next(error);
+            res.status(400).json({ error: error.message });
         });
-}
+};
+
+
