ee5208eda14edecf050a7a2d7c58a08eb614f836
Lucas Capart <lucas.capart80@gmail.com>
2023-07-03 12:31:26 +0200

    Update book.js
    Ligne 76 : modification du .findOne -> find()

--- Backend/controllers/book.js
+++ Backend/controllers/book.js
@@ -73,16 +73,65 @@ exports.deleteBooks = (req, res, next) => {
 };
 
 exports.getBestRatedBooks = (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
+    Book.find()
         .sort({ averageRating: -1 })
         .limit(3)
         .exec()
         .then((books) => {
-            res.status(200).json({ books });
+            res.status(200).json(books);
         })
         .catch((error) => {
             res.status(400).json({ error: error.message });
         });
 };
+
+
+exports.ratingBook = (req, res, next) => {
+    const userId = req.body.userId; // Récupère l'ID de l'utilisateur depuis le corps de la requête
+    const rating = parseInt(req.body.rating); // Récupère la note depuis le corps de la requête et la convertit en entier
+
+    if (isNaN(rating) || rating < 0 || rating > 5) {
+        // Vérifie si la note est invalide
+        return res
+            .status(400)
+            .json({ message: "La note doit être comprise entre 0 et 5." });
+    }
+
+    Book.findOne({ _id: req.params.id })
+        .then((book) => {
+            if (!book) {
+                // Vérifie si le livre n'existe pas
+                return res.status(404).json({ message: "Livre non trouvé." });
+            }
+
+            const existingRating = book.ratings.find(
+                (rating) => rating.userId === userId
+            ); // Vérifie s'il existe déjà une note de cet utilisateur pour ce livre
+
+            if (existingRating) {
+                // Si une note existe déjà, renvoie une erreur
+                return res
+                    .status(400)
+                    .json({ message: "L'utilisateur a déjà noté ce livre." });
+            }
+
+            book.ratings.push({ userId, grade: rating }); // Ajoute la nouvelle note au tableau "ratings" du livre
+
+            const sumOfRates = book.ratings.reduce(
+                (sum, rating) => sum + rating.grade,
+                0
+            ); // Calcule la somme des notes existantes
+
+            const averageRating = sumOfRates / book.ratings.length; // Calcule la note moyenne en divisant la somme par le nombre de notes
+            book.averageRating = Math.round(averageRating); // Arrondit la note moyenne à l'entier le plus proche
+
+            book
+                .save() // Enregistre le livre mis à jour dans la base de données
+                .then(() => res.status(200).json(book)) // Renvoie une réponse avec le livre mis à jour en cas de succès
+                .catch((error) => res.status(500).json({ error })); // Renvoie une réponse avec une erreur en cas d'échec de la sauvegarde
+        })
+        .catch((error) => res.status(500).json({ error })); // Renvoie une réponse avec une erreur en cas d'erreur lors de la recherche du livre
+};
+
 
 
