ef75d4796b8e1b51917f09ecc4386b8d9151c7e8
Lucas Capart <lucas.capart80@gmail.com>
2023-07-03 12:30:41 +0200

    Update app.js
    Suppression du console.log

--- Backend/app.js
+++ Backend/app.js
@@ -21,8 +21,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-console.log('test')
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
