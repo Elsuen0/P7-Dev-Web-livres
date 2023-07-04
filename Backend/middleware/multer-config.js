d8bdcadd46b9e74f6e5c4315e6710009c6970a85
Lucas Capart <lucas.capart80@gmail.com>
2023-07-04 15:38:43 +0200

    Update multer-config.js
    Ajout commentaires

--- Backend/middleware/multer-config.js
+++ Backend/middleware/multer-config.js
@@ -1,19 +1,24 @@
 const multer = require('multer');
 
+// Définition des types MIME acceptés avec les extensions correspondantes
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
 };
 
+// configuration du stockage des fichiers 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
+
+        // Répertoire de destination 
         callback(null, 'images');
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
+        // Génération d'un nom de fichier unique 
+        const name = file.originalname.split(' ').join('_'); // Remplacement des espaces par des underscore
+        const extension = MIME_TYPES[file.mimetype]; // Récupération de l'extension correspondante
+        callback(null, name + Date.now() + '.' + extension); // Création du nom + date + .extension 
     }
 });
 
