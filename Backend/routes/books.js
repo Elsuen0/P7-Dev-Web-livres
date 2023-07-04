0e9aedf4837c7d883b2be82612a3fea2dcd3be4d
Lucas Capart <lucas.capart80@gmail.com>
2023-07-04 12:39:21 +0200

    Update books.js
    Modification des routes pour ajouter le 'GreenCode'

--- Backend/routes/books.js
+++ Backend/routes/books.js
@@ -4,12 +4,14 @@ const bookCtrl = require('../controllers/book');
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const convertToAvif = require('../middleware/sharp');
+
 
 router.get('/', bookCtrl.displayBooks);
-router.post('/', auth, multer, bookCtrl.addBook);
+router.post('/', auth, multer, convertToAvif, bookCtrl.addBook);
 router.get('/bestrating', bookCtrl.getBestRatedBooks);
 router.get('/:id', bookCtrl.getBookById);
-router.put('/:id', auth, bookCtrl.updateOne)
+router.put('/:id', auth, multer, convertToAvif, bookCtrl.updateOne)
 router.delete('/:id', auth, bookCtrl.deleteBooks);
 router.post('/:id/rating', auth, bookCtrl.ratingBook);
 
