4fe2c985c95d8dd5c978d321b0cfc8906333080a
Lucas Capart <lucas.capart80@gmail.com>
2023-07-17 13:36:38 +0200

    Update auth.js
    Ajout du middleware 'checkRegistrationData' qui permet de vérifier si l'adresse mail et le mot de passe sont corrects

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const authCtrl = require('../controllers/auth')
+const authCtrl = require('../controllers/auth');
+const checkRegistrationData = require('../middleware/validator');
 
-router.post('/signup', authCtrl.signupUser);
+router.post('/signup', checkRegistrationData, authCtrl.signupUser);
 router.post('/login', authCtrl.loginUser);
 
 module.exports = router;
