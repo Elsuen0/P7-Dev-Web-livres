440b9610df9e432f522189b25064297021d309b1
Lucas Capart <lucas.capart80@gmail.com>
2023-07-17 13:38:15 +0200

    Create rateLimit.js
    Création du Middleware 'rateLimit' qui limite le nombre de requête sur le site et évite ainsi différentes attaques

---
+++ Backend/middleware/rateLimit.js
@@ -0,0 +1,10 @@
+const rateLimit = require('express-rate-limit');
+
+// Définition des options de la limite de fréquence
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // Période de temtps en millisecondes (15 minutes)
+    max: 100, // Nombre max de requêtes autorisées par fenêtre 
+    message: 'Trop de requête depuis cette adresse IP, veuillez réessayer dans quelques minutes'
+});
+
+module.exports = limiter;
